77c06343fba53a189832734484e7e73fedb4bb88
d33pk3rn3l <5982817+d33pk3rn3l@users.noreply.github.com>
2023-11-17 11:09:12 +0100

    pep8 conformity

--- webview_plotly.py
+++ webview_plotly.py
@@ -1,13 +1,36 @@
+import os
 import sys
 import webview
-import os
+
 
 def delete_temp_file(html_file_path):
-    # Delete the temporary HTML file
+    """
+    Delete the specified temporary HTML file.
+
+    Parameters:
+    html_file_path (str): The file path of the HTML file to be deleted.
+
+    Returns:
+    None
+    """
     if os.path.exists(html_file_path):
         os.remove(html_file_path)
 
-if __name__ == "__main__":
+
+def main():
+    """
+    Main function to create and display a webview window.
+
+    Reads an HTML file specified in the command line arguments, creates
+    a webview window with this HTML content, and deletes the file after
+    the window is closed.
+
+    Command Line Arguments:
+    1st argument (optional): File path to the HTML file.
+
+    Returns:
+    None
+    """
     # Get the path to the HTML file from the command line arguments
     html_file_path = sys.argv[1] if len(sys.argv) > 1 else ""
 
@@ -23,5 +46,9 @@ if __name__ == "__main__":
     # Start the webview and wait until the window is closed
     webview.start(debug=True)
 
-    # After the window is closed, delete the temporary file and signal completion
+    # After the window is closed, delete the temporary file
     delete_temp_file(html_file_path)
+
+
+if __name__ == "__main__":
+    main()
